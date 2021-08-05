69ae94817b3b13dc4db69db34edbca011e0382f6
maxibor <maxime.borry@gmail.com>
2021-08-05 16:35:24 +0200

    add tqdm dep

--- setup.py
+++ setup.py
@@ -10,11 +10,7 @@ setup(
     long_description_content_type="text/markdown",
     license="MIT",
     python_requires=">=3.6",
-    install_requires=[
-        "pysam",
-        "tqdm",
-        "click"
-    ],
+    install_requires=["pysam", "tqdm", "click"],
     packages=find_packages(include=["bamaligncleaner"]),
     entry_points={"console_scripts": ["bamAlignCleaner= bamaligncleaner.cli:cli"]},
 )
