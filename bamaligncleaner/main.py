d85b3e97c2fe96344e285ff7b2ddb3b4b40d9944
Alex Huebner <alexhbnr@gmail.com>
2024-07-24 10:22:10 +0200

    Add functionality to split the BAM file into multiple output files

--- bamaligncleaner/main.py
+++ bamaligncleaner/main.py
@@ -1,8 +1,10 @@
-import pysam
 import logging
-from tqdm import tqdm
 import sys
+
+import numpy as np
+import pysam
 from pysam.utils import SamtoolsError
+from tqdm import tqdm
 
 
 def bam_index(alignment, filetype):
@@ -29,12 +31,15 @@ def bam_index(alignment, filetype):
         sys.exit(1)
 
 
-def filter_bam(bam, method, output):
+def filter_bam(bam, method, output, splits):
     """Filter bam file to remove unaligned references
 
     Args:
         bam (str): Path to alignement file
+        method (str): Method to infer references present in the input alignment
+                      file
         output (str): Path to output alignment file
+        splits (int): Number of output alignment files
     """
 
     logging.basicConfig(level=logging.INFO, format="%(message)s")
@@ -81,24 +86,77 @@ def filter_bam(bam, method, output):
         reflens.append(alignment.get_reference_length(ref))
 
     logging.info("Step 3/4: recreating header")
-    header = alignment.header.to_dict()
-    header["SQ"] = [{"SN": r, "LN": rl} for (r, rl) in zip(refs, reflens)]
-    header = pysam.AlignmentHeader.from_dict(header)
+    if splits == 1:
+        header = alignment.header.to_dict()
+        header["SQ"] = [{"SN": r, "LN": rl} for (r, rl) in zip(refs, reflens)]
+        header = pysam.AlignmentHeader.from_dict(header)
+    else:  # split into multiple headers
+        header = alignment.header.to_dict()
+        splitted_sqs = split_contigs(header["SQ"], splits)
+        splits_ref_map = {
+            ref['SN']: i
+            for i, h in enumerate(splitted_sqs)
+            for ref in h
+        }
+        headers = [header.copy() for i in range(splits)]
+        for i in range(splits):
+            headers[i]['SQ'] = splitted_sqs[i]
+        headers = [pysam.AlignmentHeader.from_dict(h) for h in headers]
 
     logging.info("Step 4/4: Writing alignment file")
-    outbam = pysam.AlignmentFile(output, "wb", header=header)
-    for ref in tqdm(refs, unit="references"):
-        for read in alignment.fetch(ref):
-            read_dict = read.to_dict()
-            read_out = pysam.AlignedSegment.from_dict(read_dict, header)
-            outbam.write(read_out)
-    alignment.close()
-    outbam.close()
-    if output == "-":
-        output_message = "STDOUT"
-    else:
+    if splits == 1:
+        outbam = pysam.AlignmentFile(output, "wb", header=header)
+        for ref in tqdm(refs, unit="references"):
+            for read in alignment.fetch(ref):
+                read_dict = read.to_dict()
+                read_out = pysam.AlignedSegment.from_dict(read_dict, header)
+                outbam.write(read_out)
+        alignment.close()
+        outbam.close()
+        if output == "-":
+            output_message = "STDOUT"
+        else:
+            output_message = output
+    else:  # Split in multiple BAM files
+        if output.split(".")[-1] in ['bam', 'cram']:
+            outprefix = ".".join(output.split(".")[:-1])
+        else:
+            outprefix = bam
+        outbam = [pysam.AlignmentFile(f"{outprefix}.{i:02d}.{filetype}",
+                                      "wb", header=headers[i])
+                  for i in range(splits)]
+        for ref in tqdm(refs, unit="references"):
+            for read in alignment.fetch(ref):
+                read_dict = read.to_dict()
+                read_out = pysam.AlignedSegment.from_dict(read_dict,
+                                                          headers[splits_ref_map[ref]])
+                outbam[splits_ref_map[ref]].write(read_out)
+        alignment.close()
+        for i in range(splits):
+            outbam[i].close()
         output_message = output
     logging.info(
         f"* {total_refs - len(present_refs)} references with unaligned reads were removed from index"
     )
     logging.info(f"* Output bam file written to: {output_message}")
+
+
+def split_contigs(sq, splits):
+    """Split header of contigs into multiple dictionary so that every one has
+       a similar total length of contigs
+    Args:
+        sq(dict): Dictionary of reference contigs
+    Returns:
+        list: List of dictionary of reference contigs
+    """
+    reflens = np.asarray([ref['LN'] for ref in sq])
+    cumsum_rl = np.cumsum(reflens)
+    split_sum = cumsum_rl[-1] // splits
+    cumsum_splits = np.array(range(1, splits+1)) * split_sum
+    indices = np.searchsorted(cumsum_rl, cumsum_splits)
+    sq_splits = []
+    for i in range(splits):
+        sq_splits.append([])
+    for j, ref in enumerate(sq):
+        sq_splits[np.argmax(j < indices)].append(ref)
+    return sq_splits
